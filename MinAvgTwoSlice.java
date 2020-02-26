4b1fa55bca05134cfec01e145e3230f04eda984a
LMATER Moulay Abdellatif <52265304+lmater@users.noreply.github.com>
2020-02-26 20:44:42 +0100

    Update MinAvgTwoSlice.java

--- MinAvgTwoSlice.java
+++ MinAvgTwoSlice.java
@@ -1,62 +1,31 @@
 package org.lmater;
 
-import java.util.stream.IntStream;
-
 public class MinAvgTwoSlice {
 
 	public int solution(int[] A) {
-		int n = A.length;
-		int index = 0;
-		double minav = (double) IntStream.of(A).sum() / n;
-		double[][] avarages = new double[n - 1][n - 1];
-		double[] minAvarages = new double[n - 1];
-		avarages[0][0] = minav;
-		minAvarages[0] = minav;
-
-		for (int i = 0; i < n - 2; i += 2) {
-			for (int j = i + 1; j < n - 1; j++) {
-				avarages[i][j] = (double) (avarages[i][j - 1] * (n - (j - 1)) - A[n - (j - i)]) / (n - j);
-				avarages[i + 1][j] = (double) (avarages[i][j - 1] * (n - j + 1) - A[i]) / (n - j);
-				if (minAvarages[i] > avarages[i][j]) {
-					minAvarages[i] = avarages[i][j];
-				}
-				if (minAvarages[i + 1] >= avarages[i + 1][j] || j == (i + 1)) {
-					minAvarages[i + 1] = avarages[i + 1][j];
-				}
-				if (minav > minAvarages[i]) {
-					minav = minAvarages[i];
+		
+		int index = 0, prefix = 0;
+		double minAv = (double) (A[0] + A[1]) / 2;
+		
+		for (int i = 0; i < A.length - 1; i++) {
+			minAv = Math.min(minAv, (double) (prefix + A[i] + A[i + 1]) / (i - index + 2));
+			if (((double) (A[i] + A[i + 1]) / 2) < minAv) {
+				index = i;
+				prefix = A[index];
+				minAv = (double) (A[index] + A[index + 1]) / 2;
+				continue;
+			}
+			if (i < A.length - 2) {
+				minAv = Math.min(minAv, (double) (prefix + A[i] + A[i + 1] + A[i + 2]) / (i - index + 3));
+				if (((double) (A[i] + A[i + 1] + A[i + 2]) / 3) < minAv) {
 					index = i;
-				}
-				if (minav > minAvarages[i + 1]) {
-					minav = minAvarages[i + 1];
-					index = i + 1;
-				}
-			}
-
-			if (i + 2 <= n - 2) {
-				avarages[i + 2][i + 2] = (double) (avarages[i + 1][i + 1] * (n - (i + 1)) - A[i + 1]) / (n - (i + 2));
-
-				minAvarages[i + 2] = avarages[i + 2][i + 2];
-				if (minav > minAvarages[i + 2]) {
-					minav = minAvarages[i + 2];
-					index = i + 2;
+					prefix = A[i];
+					minAv = ((double) (A[index] + A[index + 1] + A[index + 2]) / 3);
+					continue;
 				}
 			}
+			prefix += A[i];
 		}
-		for (int i = 0; i < n - 1; i++) {
-			for (int j = 0; j < n - 1; j++) {
-				System.out.print(avarages[i][j] + "  ");
-			}
-			System.out.println();
-		}
-		System.out.println();
-		for (int i = 0; i < n - 1; i++) {
-			System.out.print(minAvarages[i] + "  ");
-
-		}
-		System.out.println();
-		System.out.println();
-		System.out.println("maxav " + minav + " index " + index);
 		return index;
 	}
 }
