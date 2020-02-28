0204078708383a97d6db3ffdbe82d407fdf44d3d
LMATER Moulay Abdellatif <52265304+lmater@users.noreply.github.com>
2020-02-28 18:30:33 +0100

    Update PassingCars.java

--- PassingCars.java
+++ PassingCars.java
@@ -1,6 +1,7 @@
 package org.lmater;
 
 public class PassingCars {
+	
 	public int solution(int[] A) {
 
 		int passingCars = 0, threshold = 1_000_000_001, prod = 0;
@@ -8,8 +9,8 @@ public class PassingCars {
 		for (int i = 0; passingCars < threshold && i < A.length; i++) {
 			passingCars += (prod * A[i]);
 			prod += (A[i] == 0) ? 1 : 0;
-
 		}
+		
 		return passingCars > threshold ? -1 : passingCars;
 	}
 }
