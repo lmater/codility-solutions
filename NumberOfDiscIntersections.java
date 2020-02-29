22a2f9e984451cc966e2fce417f5b890fc582d1b
LMATER Moulay Abdellatif <52265304+lmater@users.noreply.github.com>
2020-02-29 20:38:32 +0100

    Update NumberOfDiscIntersections.java

--- NumberOfDiscIntersections.java
+++ NumberOfDiscIntersections.java
@@ -11,55 +11,28 @@ public class NumberOfDiscIntersections {
 		int unorderedPairs = 0;
 		int exceeds = 10_000_000;
 
-		Disc[] list = new Disc[A.length];
+		long[][] list = new long[A.length][2];
 		for (int i = 0; i < A.length; i++) {
-			list[i] = new Disc(((long) i - (long) A[i]), ((long) i + (long) A[i]));
+			list[i][0] = i - (long) A[i];
+			list[i][1] = i + (long) A[i];
 		}
-		Arrays.sort(list);
+		Arrays.sort(list, (l1, l2) -> {
+			if (l1[1] > l2[1])
+				return -1;
+			return 1;
+		});
 
 		for (int i = 0; i < A.length - 1; i++) {
 			for (int j = i + 1; j < A.length; j++) {
-				if (list[i].intersect(list[j])) {
+				if (list[i][0] <= list[j][1]) {
 					unorderedPairs++;
+					if (unorderedPairs > exceeds)
+						return -1;
 				} else
 					break;
-				if (unorderedPairs > exceeds)
-					return -1;
 			}
 		}
 
 		return unorderedPairs;
-	}
-
-	class Disc implements Comparable<Disc> {
-		private final Long right;
-		private final Long left;
-
-		public Disc(Long left, Long right) {
-			this.right = right;
-			this.left = left;
-		}
-
-		private Long getRight() {
-			return right;
-		}
-
-		private Long getLeft() {
-			return left;
-		}
-
-		@Override
-		public String toString() {
-			return getLeft() + "," + getRight();
-		}
-
-		@Override
-		public int compareTo(Disc o) {
-			return (int) (o.getRight() - this.getRight());
-		}
-
-		public boolean intersect(Disc o) {
-			return (o.getRight() >= this.getLeft());
-		}
 	}
 }
