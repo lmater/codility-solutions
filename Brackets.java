6e0d16c74de1545864600dbef2fe785c104b6c2f
LMATER Moulay Abdellatif <52265304+lmater@users.noreply.github.com>
2020-03-01 20:40:07 +0100

    Update Brackets.java

--- Brackets.java
+++ Brackets.java
@@ -6,33 +6,30 @@ import java.util.Deque;
 public class Brackets {
 
 	public int solution(String S) {
-		Deque<Character> queue = new ArrayDeque<Character>();
-		int bracket;
+				Deque<Character> queue = new ArrayDeque<Character>();
+		String bracket;
 		char[] array = S.toCharArray();
-		if (array.length % 2 != 0)
+		int n = array.length;
+		if (n % 2 != 0)
 			return 0;
-		String str = "";
-		for (int i = 0; i < array.length; i++) {
-//			System.out.println((int) array[i]);
 
-			Character front = queue.peek();
-			bracket = ((front == null) ? array[i] : front + array[i]);
-			System.out.println(front + " " + array[i] + " " + bracket + " i=" + i + " queue.size()=" + queue.size());
-			if (queue.size() > (array.length - i + 1) || bracket == 41 || bracket == 93 || bracket == 125
-					|| bracket == 132 || bracket == 133 || bracket == 216 || bracket == 164 || bracket == 165)
+		if (array.length > 0)
+			if (array[n - 1] == '(' || array[n - 1] == '{' || array[n - 1] == '[') {
 				return 0;
-			else if (bracket == 248 || bracket == 81 || bracket == 184)
+			}
+		for (int i = 0; i < n; i++) {
+			Character front = queue.peek();
+			bracket = ((front == null) ? "" + array[i] : front.toString() + "" + array[i]);
+			if (queue.size() > (n - i + 1) || bracket.equals(")") || bracket.equals("}") || bracket.equals("]")
+					|| bracket.equals("(}") || bracket.equals("(]") || bracket.equals("{]") || bracket.equals("[)")
+					|| bracket.equals("{)") || bracket.equals("[}"))
+				return 0;
+			else if (bracket.equals("()") || bracket.equals("{}") || bracket.equals("[]"))
 				queue.poll();
 			else {
 				queue.push(array[i]);
 			}
-			str += array[i];
-//			System.out.println(S);
-//			System.out.println(str);
 		}
-//		System.out.println(str);
-
-		// "(", "{", "[", "]", "}",")".
 		return 1;
 	}
 }
