bffe515ffa5feb9d7049cc888e140a65a636910b
LMATER Moulay Abdellatif <52265304+lmater@users.noreply.github.com>
2020-03-02 22:56:59 +0100

    Update Fish.java

--- Fish.java
+++ Fish.java
@@ -3,33 +3,33 @@ package org.lmater;
 import java.util.Stack;
 
 public class Fish {
-	
+
 	public int solution(int[] A, int[] B) {
 
 		int n = A.length;
-
-		Stack<Integer> upstream = new Stack<Integer>();
+		int upStreamCount = 0;
 		Stack<Integer> downstream = new Stack<Integer>();
 
 		for (int i = 0; i < n; i++) {
 			if (B[i] == 0) {
-				
+
 				while (!downstream.empty()) {
 					if (downstream.peek() < A[i])
 						downstream.pop();
 					else
 						break;
 				}
-				
+
 				if (downstream.empty()) {
-					upstream.push(A[i]);
+					upStreamCount++;
 				}
-				
+
 			} else {
 				downstream.push(A[i]);
 			}
 		}
-		
-		return upstream.size() + downstream.size();
+
+		return upStreamCount + downstream.size();
 	}
+
 }
