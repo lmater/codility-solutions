4f63a146885c272c24ce28acd8a302078d8577af
LMATER Moulay Abdellatif <52265304+lmater@users.noreply.github.com>
2020-03-10 15:26:37 +0100

    Update Dominator.java

--- Dominator.java
+++ Dominator.java
@@ -4,28 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Dominator {
-	public int solution(int[] A) {
-		int index = -1;
+	
+	public int solution(int[] A) {		
 		int n = A.length;
+		Map<Integer, Integer> stock = new HashMap<Integer, Integer>();
+		int index = -1;
 		int CountLeader = 0;
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-		for (int i = 0; i < A.length; i++) {
+
+		for (int i = 0; CountLeader <= (n / 2) && i < A.length; i++) {
 			int count = 1;
-			if (map.containsKey(A[i])) {
-				count = map.get(A[i]);
-				count++;
-				map.put(A[i], count);
+			if (stock.containsKey(A[i])) {
+				count += stock.get(A[i]);
+				stock.put(A[i], count);
 			} else {
-				map.put(A[i], 1);
+				stock.put(A[i], 1);
 			}
 			if (CountLeader < count) {
 				CountLeader = count;
 				index = i;
 			}
-			if (CountLeader > (n / 2))
-				break;
-//			System.out.println(map);
-//			System.out.println(n / 2);
 		}
 		return (CountLeader > (n / 2)) ? index : -1;
 	}
